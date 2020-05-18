4ae494a687a0e64c2720c16fd89cb73a7fac5d3e
luizfelipej <luizfelipejesus94@gmail.com>
2020-05-18 12:44:51 -0300

    Placing pieces in the board

---
+++ src/chess/King.java
@@ -0,0 +1,16 @@
+package chess;
+
+import boardgame.Board;
+
+public class King extends ChessPiece{
+
+	public King(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+
+}
---
+++ src/chess/Rook.java
@@ -0,0 +1,16 @@
+package chess;
+
+import boardgame.Board;
+
+public class Rook extends ChessPiece{
+
+	public Rook(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "R";
+	}
+
+}
