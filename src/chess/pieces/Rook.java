c235580f401bf4990214b6fd4f076ed473a39308
luizfelipej <luizfelipejesus94@gmail.com>
2020-05-20 12:25:57 -0300

    Printing possible moves, and rook moves correction

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -43,7 +43,7 @@ public class Rook extends ChessPiece {
 		}
 
 		// Right
-		p.setValues(position.getRow(), position.getColumn() - 1);
+		p.setValues(position.getRow(), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() + 1);
@@ -52,11 +52,11 @@ public class Rook extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// Above
+		// down
 		p.setValues(position.getRow() + 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1);
+			p.setRow(p.getRow() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
